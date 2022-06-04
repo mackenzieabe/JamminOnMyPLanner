502bce9b651e5ed51d60786c7c2a2d35fa5b0a9c
mackgabe <mackenzieabe@gmail.com>
2022-06-04 18:41:17 -0400

    added weekday and tried adding checkTime function so that the time blocks are color coded for past, present and future

--- Develop/assets/js/planner.js
+++ Develop/assets/js/planner.js
@@ -3,11 +3,41 @@ var saveBtn= document.querySelector(".save-btn")
 var currentDay=document.querySelector("#currentDay")
 //var day= ["Sunday", "Monday","Tuesday","Wednesday", "Thursday","Friday","Saturday"]
 //document.getElementById('currentDay').innerHTML= day[""];
-const currentDate = moment().format('MM/DD/YYYY'); // get current date
-document.getElementById("currentDay").textContent = currentDate;
-const dayName = getDayName(dayIndex)
 
+//const currentDate = moment().format('MM/DD/YYYY'); // get current date
+//document.getElementById("currentDay").textContent = currentDate;
+//const dayName = getDayName(dayIndex)
 
+var weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+var d = new Date();
+let day = weekday[d.getDay()];
+console.log(weekday);
+$('#currentDay').html("Today is: " + day);
+
+//var today= new Date();
+//console.log(today);
+
+var checkTime= function() {
+    var hour= $(".hour").text().trim();
+
+    var time= moment(hour, "LT");
+    console.log(time)
+
+    if (moment().isAfter(time)) {
+        $(".hour").addClass(".past");
+    } else if (moment().isBefore(time)) {
+        $(".hour").addClass(".future");
+    } else {
+        $(".hour").addClass(".present");
+    }
+}
+checkTime();
+console.log(checkTime);
+
+function setColor(element, color) {
+  element.style.backgroundColor = color;
+}
 
 input.innerHTML= localStorage.getItem('value');
 
@@ -16,6 +46,5 @@ saveBtn.onclick=function() {
         localStorage.getItem('value')
     
     };
-
 
 
